dd9594044eeea5c337d7b35ac11171aecf725350
kwood <155027796+qkewq@users.noreply.github.com>
2026-04-30 13:49:07 -0700

    Initial commit

---
+++ libhasht.c
@@ -0,0 +1,198 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "libhasht.h"
+
+/* Nodes that will store the key value pairs in linked list */
+typedef struct Ht_node{
+	struct Ht_node *next;
+	char *key;
+	size_t keylen;
+	char *val;
+	size_t vallen;
+} Ht_node;
+
+int ht_init(Hashtable **res, size_t nelements){
+	if(!res){
+		return EPTRINVALD;
+	}
+	if(!nelements){
+		return ENELINVALD;
+	}
+	Hashtable *ht = calloc(1, sizeof(Hashtable));
+	Ht_node **nodes = calloc(nelements, sizeof(Ht_node *));
+	if(!ht || !nodes){
+		free(ht);
+		free(nodes);
+		return EMEMFAILED;
+	}
+	ht->nnodes = nelements;
+	ht->nodes = nodes;
+	*res = ht;
+	return SUCCESSFUL;
+}
+
+char *ht_strerror(int err){
+	switch(err){
+		case EKNOTFOUND:
+			return "EKNOTFOUND: The key was not found in the table";
+		case SUCCESSFUL:
+			return "Operation completed successfully";
+		case EPTRINVALD:
+			return "EPTRINVALD: Invalid pointer";
+		case ENELINVALD:
+			return "ENELINVALD: Invalid number of elements";
+		case EMEMFAILED:
+			return "EMEMINVALD: Memory allocation failed";
+		case EHASHFFAIL:
+			return "EHASHFFAIL: Failed to hash key";
+		case ETRUNCATED:
+			return "ETRUNCATED: Provided buffer was too small";
+		default:
+			return "Invalid error code";
+	}
+}
+
+void ht_free(Hashtable *ht){
+	if(!ht){
+		return;
+	}
+
+	Ht_node *current = NULL;
+	Ht_node *next = NULL;
+	for(int i = 0; i < ht->nnodes; i++){
+		current = ht->nodes[i];
+		while(current){
+			next = current->next;
+			free(current->key);
+			free(current->val);
+			free(current);
+			current = next;
+		}
+	}
+	free(ht->nodes);
+	free(ht);
+}
+
+int ht_hash(char *key, size_t keylen){
+	int ret = 0;
+	for(int i = 0; i < keylen; i++){
+		ret += key[i] * (i + 1); /* +1 to avoid multiplying by 0 */
+	}
+
+	return ret;
+}
+
+int ht_get_index(char *key, size_t keylen, size_t nnodes){
+	int index = ht_hash(key, keylen);
+	if(index == -1){
+		return EHASHFFAIL;
+	}
+
+	return index % (int)nnodes;
+}
+
+int ht_insert(Hashtable *ht, char *key, size_t keylen, char *val, size_t vallen){
+	if(!ht || !key){
+		return EPTRINVALD;
+	}
+
+	int index = ht_get_index(key, keylen, ht->nnodes);
+	if(index == -1){
+		return EHASHFFAIL;
+	}
+	Ht_node *new = calloc(1, sizeof(Ht_node));
+	char *stored_key = calloc(1, keylen);
+	char *stored_val = calloc(1, vallen);
+	if(!new || !stored_key || !stored_val){
+		free(new);
+		free(stored_key);
+		free(stored_val);
+		return EMEMFAILED;
+	}
+	memcpy(stored_key, key, keylen);
+	memcpy(stored_val, val, vallen);
+	new->key = stored_key;
+	new->keylen = keylen;
+	new->val = stored_val;
+	new->vallen = vallen;
+
+	new->next = ht->nodes[index];
+	ht->nodes[index] = new;
+	ht->entries++;
+
+	return SUCCESSFUL;
+}
+
+int ht_delete(Hashtable *ht, char *key, size_t keylen){
+	if(!ht || !key){
+		return EPTRINVALD;
+	}
+
+	int index = ht_get_index(key, keylen, ht->nnodes);
+	if(index == -1){
+		return EHASHFFAIL;
+	}
+
+	Ht_node *previous = ht->nodes[index];
+	Ht_node *current = previous;
+	while(current){
+		if(current->keylen != keylen){
+			previous = current;
+			current = current->next;
+			continue;
+		}
+		if(memcmp(current->key, key, keylen) == 0){
+			previous->next = current->next;
+			free(current->key);
+			free(current->val);
+			free(current);
+			ht->entries--;
+			return SUCCESSFUL;
+		}
+		else{
+			previous = current;
+			current = current->next;
+			continue;
+		}
+	}
+
+	return EKNOTFOUND;
+}
+
+int ht_lookup(Hashtable *ht, char *key, size_t keylen, char *dst, size_t *dstlen){
+	if(!ht || !key || !dst || !dstlen){
+		return EPTRINVALD;
+	}
+
+	int index = ht_get_index(key, keylen, ht->nnodes);
+	if(index == -1){
+		return EHASHFFAIL;
+	}
+
+	Ht_node *current = ht->nodes[index];
+	while(current){
+		if(current->keylen != keylen){
+			current = current->next;
+			continue;
+		}
+		if(memcmp(current->key, key, keylen) == 0){
+			if(*dstlen < current->vallen){
+				memcpy(dst, current->val, *dstlen);
+				return ETRUNCATED;
+			}
+			else{
+				memcpy(dst, current->val, current->vallen);
+				*dstlen = current->vallen;
+				return SUCCESSFUL;
+			}
+		}
+		else{
+			current = current->next;
+			continue;
+		}
+	}
+
+	return EKNOTFOUND;
+}
---
+++ libhasht.h
@@ -0,0 +1,51 @@
+#ifndef LIBHASHT_H
+#define LIBHASHT_H_
+
+#define EKNOTFOUND	-1	/* Provided key was not found in hash table */
+#define SUCCESSFUL	0	/* Successful operation */
+#define EPTRINVALD	1	/* A null pointer was passed to a function that expected a value */
+#define ENELINVALD	2	/* Value of zero passed for number of elements */
+#define EMEMFAILED	3	/* Memory allocation failed */
+#define EHASHFFAIL	4	/* The hash function failed */
+#define ETRUNCATED	5	/* The result of a lookup was truncated to fit in the provided buffer */
+
+/* Hash table type that is returned from the init function */
+/* This is passed into subsequent hash table functions */
+typedef struct Hashtable{
+	struct Ht_node **nodes;
+	size_t nnodes;
+	size_t entries;
+} Hashtable;
+
+/* On success these functions return 0 */
+/* On error they will return a positive error code */
+/* If a delete or lookup completed successfully but the key was not in the table it will return -1*/
+
+/* Initialization function for the hash table that */
+/* creates the table with nelements elements and */
+/* places a pointer to the hash table in res */
+int ht_init(Hashtable **res, size_t nelements);
+
+/* Get the string form of an error code returned by a hash table function */
+char *ht_strerror(int err);
+
+/* Free the table and all key value pairs from memory */
+void ht_free(Hashtable *ht);
+
+/* Insert the string key with length keylen paired with value with length vallen */
+/* into the table */
+int ht_insert(Hashtable *ht, char *key, size_t keylen, char *val, size_t vallen);
+
+/* Delete the specified key from the table */
+/* Returns EKNOTFOUND if the key is not in the table */
+int ht_delete(Hashtable *ht, char *key, size_t keylen);
+
+/* Lookup the specified key in the table */
+/* Returns EKNOTFOUND if the key is not found in the table */
+/* Places the value asociated with key in dst and updates dstlen with */
+/* the amount written */
+/* If dstlen is not big enough to store the value then the value is  */
+/* truncated and ETRUNCATED is returned, dstlen wont be changed if ETRUNCATED is returned */
+int ht_lookup(Hashtable *ht, char *key, size_t keylen, char *dst, size_t *dstlen);
+
+#endif
